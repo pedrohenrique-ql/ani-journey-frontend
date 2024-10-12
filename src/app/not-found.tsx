189c1490483fa48ee803f8b7b718aef91bccc195
Pedro Henrique <pedrohenriqueql.dev@gmail.com>
2024-10-12 12:02:44 -0300

    feat: create not found page layout

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,20 @@
-import Redirect from '@/components/common/Redirect';
+import Link from 'next/link';
+import React from 'react';
 
 function NotFoundPage() {
-  return <Redirect to="/" />;
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <h1 className="text-8xl font-bold">404</h1>
+      <span className="mt-4 text-2xl">Page Not Found</span>
+      <p className="mt-2">Sorry, the page you are looking for does not exist</p>
+      <Link
+        href="/"
+        className="mt-6 rounded-lg bg-blue-600 px-6 py-3 text-white transition duration-300 hover:bg-blue-700"
+      >
+        Voltar para o início
+      </Link>
+    </div>
+  );
 }
 
 export default NotFoundPage;
